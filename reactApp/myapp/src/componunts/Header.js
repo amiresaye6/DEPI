1eaf1a1df170adf6b3d1caacffcf2d07b222085e
Amir Alsayed <amyralsyd367@gmail.com>
2024-07-15 00:09:17 +0300

    firt react app

---
+++ reactApp/myapp/src/componunts/Header.js
@@ -0,0 +1,11 @@
+function Header(props) {
+    return (
+        <div className="header">
+            <img src="../images/menu.png"></img>;
+            <div className="logo">My Food</div>;
+            <div className="mail">Mail</div>;
+        </div>
+    )
+}
+
+export default Header;
